4c09af5769134e76ba4d71f6bc10a28eab442be2
Michał Płonka <mplonka1992@gmail.com>
2023-05-30 12:19:20 +0200

    fetch and myFetch custom implementation with XMLHTTPRequest

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const boxRef = document.querySelector('.box');
-const animationDuration = 3000; // Czas trwania animacji w milisekundach
+//const animationDuration = 3000; // Czas trwania animacji w milisekundach
 
 /*function animateBox() {
     setTimeout(() => {
@@ -19,7 +19,7 @@ const animationDuration = 3000; // Czas trwania animacji w milisekundach
 
 animateBox(); // Wywołanie funkcji animateBox() dla rozpoczęcia animacji
 */
-const receipt = new Promise((resolve, reject) => {
+/*const receipt = new Promise((resolve, reject) => {
     setTimeout(() => {
         resolve("Kebab maj friend!");
     }, 3001);
@@ -35,3 +35,55 @@ receipt
 .catch((msg) =>{
     console.log(msg)
 });
+ */
+// function move(top, left) {
+//     return new Promise((resolve) => {
+//         setTimeout(() => {
+//             boxRef.style.left = left;
+//             boxRef.style.top = top;
+//             resolve();
+//         }, 3000);
+//     });
+// }
+//
+// move(0, '500px')
+//     .then(() => move ('500px', '500px'))
+//     .then(() => move('500px', '0px'))
+//     .then(() => move('0px', '0px'));
+
+ const url = 'https://api.nbp.pl/api/exchangerates/rates/a/chf/?format=json';
+// const request = fetch (url);
+//
+//
+// request
+//     .then((response) => response.json())
+//     .then((data) => console.log(data.rates[0].mid))
+//     .catch(console.log);
+
+function myFetch(url){
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+
+        xhr.onload = function () {
+            if(xhr.status >= 200 && xhr.status < 400){
+                const response = JSON.parse(xhr.responseText);
+                resolve(response);
+            } else{
+                reject(xhr.status);
+            }
+        }
+        xhr.onerror = function () {
+            reject('Something is no yes!')
+        }
+        xhr.open('GET', url, true);
+        xhr.send()
+    })
+}
+
+const request = myFetch(url);
+
+request
+    .then((data) => console.log(data))
+    .catch((error) => console.log(error))
+    .finally(() => {console.log('done!')})
+
